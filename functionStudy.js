a8f83d469b43cabd79bcf8702cfaf93a9fd567aa
geunsoo96 <geunsoo1106@gmail.com>
2022-09-01 18:57:58 +0900

    추가 자습

---
+++ functionStudy.js
@@ -0,0 +1,27 @@
+// let tagList1 = function (element, index) {
+//   this.id = index;
+//   this.name = element;
+// };
+// 함수 리터럴
+
+// let tagList2 = (element, index) => {
+//   this.id = index;
+//   this.name = element;
+// };
+// 축약형
+
+// function tagList3(element, index) {
+//   this.id = index;
+//   this.name = element;
+// }
+// 선언형
+
+// function --> this를 가둠
+// => --> this를 풀어줌, 위로 이동해서 가동
+
+// 함수 작성법 세가지
+
+let a = () => {
+  return this;
+};
+console.log(a());
